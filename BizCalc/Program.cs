55fb9d7f7a5578d633653bbf94521653784cbaf5
grthvn265 <arevalo.aaroncarl26@gmail.com>
2026-05-14 00:48:29 +0800

    Add summary dialog and timezone handling to main application

--- BizCalc/Program.cs
+++ BizCalc/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Globalization;
+using System.Text;
 using System.Windows.Forms;
 
 namespace BizCalc
@@ -25,6 +24,55 @@ namespace BizCalc
             }
 
             Application.Run(new Form1());
+            ShowSummaryDialog();
+        }
+
+        private static void ShowSummaryDialog()
+        {
+            MessageBox.Show(
+                BuildSummaryText(),
+                "Program Summary",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+
+        private static readonly CultureInfo PhilippineCulture = new CultureInfo("en-PH");
+        private static readonly TimeZoneInfo PhilippineTimeZone = ResolvePhilippineTimeZone();
+
+        private static string BuildSummaryText()
+        {
+            var localNow = TimeZoneInfo.ConvertTime(DateTime.UtcNow, PhilippineTimeZone);
+            var builder = new StringBuilder();
+            builder.AppendLine("CC103");
+            builder.AppendLine("Programmed By: Meneses, Gabriel Benedict K.");
+            builder.AppendLine("Date of Submission: null");
+            builder.AppendLine("Message to Your Professor: Thank you, Mam Lizel, for your guidance, patience, and support throughout this course.");
+            builder.AppendLine($"Local Date and Time: {localNow.ToString("F", PhilippineCulture)}");
+            return builder.ToString();
+        }
+
+        private static TimeZoneInfo ResolvePhilippineTimeZone()
+        {
+            try
+            {
+                return TimeZoneInfo.FindSystemTimeZoneById("Philippine Standard Time");
+            }
+            catch (TimeZoneNotFoundException)
+            {
+                return TimeZoneInfo.CreateCustomTimeZone(
+                    "Philippine Standard Time",
+                    TimeSpan.FromHours(8),
+                    "Philippine Standard Time",
+                    "PST");
+            }
+            catch (InvalidTimeZoneException)
+            {
+                return TimeZoneInfo.CreateCustomTimeZone(
+                    "Philippine Standard Time",
+                    TimeSpan.FromHours(8),
+                    "Philippine Standard Time",
+                    "PST");
+            }
         }
     }
 }
